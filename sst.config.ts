8d8f298eb54dfb8612533855dd1b251133c302dd
Jay V <air@live.ca>
2023-11-24 16:31:05 -0500

    Adding SST

---
+++ sst.config.ts
@@ -0,0 +1,24 @@
+import { SSTConfig } from 'sst';
+import { NextjsSite } from 'sst/constructs';
+
+export default {
+  config(_input) {
+    return {
+      name: 'next-partial-prerendering',
+      region: 'us-east-1',
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, 'site', {
+        experimental: {
+          streaming: true,
+        },
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
